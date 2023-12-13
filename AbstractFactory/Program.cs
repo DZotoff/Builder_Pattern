0d78e854e846789965426ebc2667253f9c3ee627
Daniil Zotov <ddazo002@edu.xamk.fi>
2023-12-13 22:57:20 +0200

    Add project files.

---
+++ AbstractFactory/Program.cs
@@ -0,0 +1,141 @@
+﻿namespace DesignPatterns
+{
+    // Class for Printer
+    // TODO#1: Convert to use Singleton pattern
+    public class Printer
+    {
+        private static Printer instance;
+
+        private Printer() { }
+
+        public static Printer GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Printer();
+            }
+            return instance;
+        }
+
+        public void Print(string message)
+        {
+            // Output: print out the string message 
+            Console.WriteLine(message);
+         
+        }
+    }
+
+    // Class template for Exam classes
+    // TODO#2: Convert to use Abstract Factory pattern
+    // Create an Exam interface and an Abstract Factory to manage the creation of different exam types.
+    public interface Exam
+    {
+        void Conduct();
+        void Evaluate();
+        void PublishResults();
+    }
+
+    public class MathExam : Exam
+    {
+        public void Conduct()
+        {
+            // Output: "Conducting Math Exam", should use Printer class to print the message
+            Printer printer = Printer.GetInstance();
+            printer.Print("Conducting Math Exam");
+
+        }
+
+        public void Evaluate()
+        {
+            // Output: "Evaluating Math Exam", should use Printer class to print the message
+            Printer printer = Printer.GetInstance();
+            printer.Print("Evaluating Math Exam");
+        }
+
+        public void PublishResults()
+        {
+            // Output: "Publishing Math Exam Results", should use Printer class to print the message
+            Printer printer = Printer.GetInstance();
+            printer.Print("Publishing Math Exam Results");
+        }
+    }
+
+    // TODO#5: Add new ScienceExam class
+    public class ScienceExam : Exam
+    {
+        public void Conduct()
+        {
+            Printer printer = Printer.GetInstance();
+            printer.Print("Conducting Science Exam");
+        }
+
+        public void Evaluate()
+        {
+            Printer printer = Printer.GetInstance();
+            printer.Print("Evaluating Science Exam");
+        }
+
+        public void PublishResults()
+        {
+            Printer printer = Printer.GetInstance();
+            printer.Print("Publishing Science Exam Results");
+        }
+    }
+
+    // TODO#6: Add another ProgrammingExam class
+    public class ProgrammingExam : Exam
+    {
+        public void Conduct()
+        {
+            Printer printer = Printer.GetInstance();
+            printer.Print("Conducting Programming Exam");
+        }
+
+        public void Evaluate()
+        {
+            Printer printer = Printer.GetInstance();
+            printer.Print("Evaluating Programming Exam");
+        }
+
+        public void PublishResults()
+        {
+            Printer printer = Printer.GetInstance();
+            printer.Print("Publishing Programming Exam Results");
+        }
+    }
+
+    // Main Program
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO#7: Initialize Printer that uses Singleton pattern
+            Printer printer = Printer.GetInstance();
+
+            // TODO#8: Test that the created Printer works, by calling the Print method
+            printer.Print("Testing Printer");
+            // TODO#9: Ensure that only one Printer instance is used throughout the application.
+            //         Try to create new Printer object and compare the two objects to check,
+            //         that the new printer object is the same instance
+            Printer anotherPrinter = Printer.GetInstance();
+            Console.WriteLine("Are the two printer instances the same? " + (printer == anotherPrinter));
+            // TODO#10: Use Abstract Factory to create different types of exams.
+            Exam mathExam = new MathExam();
+            Exam scienceExam = new ScienceExam();
+            Exam programmingExam = new ProgrammingExam();
+
+            mathExam.Conduct();
+            mathExam.Evaluate();
+            mathExam.PublishResults();
+
+            scienceExam.Conduct();
+            scienceExam.Evaluate();
+            scienceExam.PublishResults();
+
+            programmingExam.Conduct();
+            programmingExam.Evaluate();
+            programmingExam.PublishResults();
+        }
+    }
+
+}
---
+++ BuilderAssignment/Models/RamenBowl.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderAssignment.Models
+{
+    /// <summary>
+    /// This class represents the final bowl of ramen
+    /// It should have attributes like broth, type of noodle, toppings and extras
+    /// </summary>
+    public class RamenBowl
+    {
+        //TODO#1: Add attributes for each enum in IngredientEnums.cs file
+
+        //TODO#2: Return description of finished bowl of ramen
+
+        public BrothType Broth { get; set; }
+        public NoodleType Noodles { get; set; }
+        public Topping[] Toppings { get; set; }
+        public Extra[] Extras { get; set; }
+        public static string FinishedBowlOfRamen(RamenBowl ramen)
+        {
+            StringBuilder description = new StringBuilder();
+
+            description.AppendLine($"Broth: {ramen.Broth}");
+            description.AppendLine($"Noodles: {ramen.Noodles}");
+
+            if (ramen.Toppings != null && ramen.Toppings.Length > 0)
+            {
+                description.AppendLine("Toppings: " + string.Join(", ", ramen.Toppings));
+            }
+
+            if (ramen.Extras != null && ramen.Extras.Length > 0)
+            {
+                description.AppendLine("Extras: " + string.Join(", ", ramen.Extras));
+            }
+            return description.ToString();
+        }
+    }
+}
